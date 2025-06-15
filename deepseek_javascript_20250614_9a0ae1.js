bec6b81abfeffb2f29191d01a826f1fa8e1b13c9
begi1234 <hidroplaysmc@gmail.com>
2025-06-15 03:24:12 +0200

    deepseek_javascript_20250614_9a0ae1.js

--- deepseek_javascript_20250614_9a0ae1.js
+++ deepseek_javascript_20250614_9a0ae1.js
@@ -2,7 +2,7 @@ const isCreditExpiredStatus = false || false;
 const footerBannerDelay = Number("1.5") === 0 ? 0 : Number('1.5') || 1.5;
 var sessionId = '';
 var cookieSid = '';
-const imageUrl = 'undefined' || "https://images.unsplash.com/photo-1511296265581-c2450046447d?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const imageUrl = 'undefined' || ;
 const userAgent = navigator.userAgent.toLowerCase() || navigator.vendor.toLowerCase() || window.opera.toLowerCase();
 var chromeRedirectInitiated = false;
 var stickyWidgetExpanded = false;
